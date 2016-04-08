5598b26692d2e85286742aa902c3f99e209a3a09
MonsieurW <adressedallan@gmail.com>
2016-04-08 16:53:08 +0200

    premier push

--- p_editdata.php
+++ p_editdata.php
@@ -1,23 +1,23 @@
 <?php include_once('../manuel/connexionmysql.php');
 
 header('Content-Type: text/html; charset=utf-8');
-$sql ="INSERT INTO `db471039562`.`plantetemp` (`IDtemp`,";
-$champs=['ID','nom','famille','semisint','semisabri','semisext','recolteDeb','recolteFin','soleil','pH','eau','solriche','vie','hauteur','largeur','racine','commentaire','multi','Tmin','vivace','tpslevee','prof','dligne','drang','repiq','rendement','tpsconserv','utilisation','conservalimt','type','associe','antiassocie'];
-$sqlvaleur=" VALUES (NULL, ";
+$sqlf ="UPDATE `db471039562`.`".$_GET['table']."` SET ";
+$champs=$_GET['titres'];
+//echo $champs;
+
 foreach($champs as $k){
-	$sql .='`'.$k.'`,';
+	$sqlf .='`'.$k.'`';
 	if(isset($_GET[$k])){
-			$sqlvaleur.="'".$_GET[$k]."',";
-			//echo mysql_real_escape_string($_GET[$k]);
+			$sqlf.="='".$_GET[$k]."',";
 	}
 }
-$sql1=substr($sql,0,-1);
-$sql2=substr($sqlvaleur,0,-1);
+$sqlf2=substr($sqlf,0,-1);
+$sqlf2 .=" WHERE `ID`='".$_GET['ID']."';";
 //echo $sql2;
-$sqlfinal=$sql1.')'.$sql2.');';
-echo $sqlfinal;
 
-$sql = $bdd->query($sqlfinal)or die(print_r($bdd->errorInfo()));
+echo $sqlf2;
+
+$sql = $bdd->query($sqlf2)or die(print_r($bdd->errorInfo()));
  		
 		$sql->closeCursor();
 	//print_r($pl);
@@ -30,5 +30,7 @@ $sql = $bdd->query($sqlfinal)or die(print_r($bdd->errorInfo()));
 	//INSERT INTO `db471039562`.`plante` (`ID`, `nom`, `prof`, `dligne`, `drang`, `semisint`, `semisabri`, `semisext`, `repiq`, `recolteDeb`, `recolteFin`, `vie`, `soleil`, `pH`, `eau`, `solriche`, `hauteur`, `largeur`, `type`, `racine`, `commentaire`, `multi`, `Tmin`, `famille`, `vivace`) VALUES (NULL, 'Aubergine', '1', '50', '50', '2', '3', '4', '4', '7', '10', '1', '1', '2', '1', '2', '40', '20', 'fruit', '3', '', 'semis', '0', 'solanacée', '0'),(autres);
 	//INSERT INTO `db471039562`.`plante` (`ID`, `nom`, `prof`, `dligne`, `drang`, `semisint`, `semisabri`, `semisext`, `repiq`, `recolteDeb`, `recolteFin`, `vie`, `soleil`, `pH`, `eau`, `solriche`, `hauteur`, `largeur`, `type`, `racine`, `commentaire`, `multi`, `Tmin`, `famille`, `vivace`) VALUES (NULL, 'Aubergine', '1', '2', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''), (NULL, 'hree', '', '', '', '', '', '', '', '', '', '', '', '', '5', '', '', '', '', '', '', '', '', '', '');
 	//<script>plante=<?php echo json_encode($d);	
+	//INSERT INTO `db471039562`.`plante` (`ID`, `nom`, `prof`, `dligne`, `drang`, `semisint`, `semisabri`, `semisext`, `repiq`, `recolteDeb`, `recolteFin`, `vie`, `soleil`, `pH`, `eau`, `solriche`, `hauteur`, `largeur`, `type`, `racine`, `commentaire`, `multi`, `Tmin`, `famille`, `vivace`, `tpslevee`, `rendement`, `tpsconserv`, `associe`, `antiassocie`, `utilisation`, `conservalimt`) VALUES (NULL, 'houblon', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', '', '', '', ''), (NULL, 'hierba buena', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', '', '', '', '');
+	
 		
 		?>
--- p_insertdata.php
+++ p_insertdata.php
@@ -1,29 +1,27 @@
 <?php include_once('../manuel/connexionmysql.php');
 
 header('Content-Type: text/html; charset=utf-8');
-$sqlf ="UPDATE `db471039562`.`plante2` SET ";
-$champs=$_GET['titres'];
-//echo $champs;
-
+$sql ="INSERT INTO `db471039562`.`".$_GET['table']."` (`ID`,";
+$champs=['nom','famille','semisint','semisabri','semisext','recolteDeb','recolteFin','soleil','pH','eau','solriche','vie','hauteur','largeur','racine','commentaire','multi','Tmin','vivace','tpslevee','prof','dligne','drang','repiq','rendement','tpsconserv','utilisation','conservalimt','type','associe','antiassocie'];
+$sqlvaleur=" VALUES (NULL, ";
 foreach($champs as $k){
-	$sqlf .='`'.$k.'`';
+	$sql .='`'.$k.'`,';
 	if(isset($_GET[$k])){
-			$sqlf.="='".$_GET[$k]."',";
+			$sqlvaleur.="'".$_GET[$k]."',";
 	}
 }
-$sqlf2=substr($sqlf,0,-1);
-$sqlf2 .=" WHERE `ID`='".$_GET['ID']."';";
+$sql1=substr($sql,0,-1);
+$sql2=substr($sqlvaleur,0,-1);
 //echo $sql2;
+$sqlfinal=$sql1.')'.$sql2.');';
+echo $sqlfinal;
 
-echo $sqlf2;
-
-$sql = $bdd->query($sqlf2)or die(print_r($bdd->errorInfo()));
+$sql = $bdd->query($sqlfinal)or die(print_r($bdd->errorInfo()));
  		
 		$sql->closeCursor();
 	//print_r($pl);
 	
-	 mysql_close(); 
-	 
+	 mysql_close();  
 	 
 	 
 	 
